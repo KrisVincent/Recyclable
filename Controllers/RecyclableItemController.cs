123df752a4ac33c02739218bf7c598d108ce7edc
Kris <87560373+KrisVincent@users.noreply.github.com>
2024-01-18 15:21:02 +0800

    Add files via upload

---
+++ Controllers/RecyclableItemController.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Recyclable.Interface;
+using Recyclable.Models;
+using Recyclable.Repository;
+using System;
+
+namespace Recyclable.Controllers
+{
+    public class RecyclableItemController : Controller
+    {
+        public IRecyclableTypeRepository recyclableTypeRepository;
+        public IRecyclableItemRepository recyclableItemRepository;
+    
+        public RecyclableItemController(IRecyclableTypeRepository _recyclableTypeRepository, IRecyclableItemRepository _recyclableItemRepository)
+        {
+            recyclableTypeRepository = _recyclableTypeRepository;
+            recyclableItemRepository = _recyclableItemRepository;
+        }
+        // GET: RecyclableItemController
+        public ActionResult List()
+        {
+            IEnumerable<RecyclableItem> recyclableItems = recyclableItemRepository.GetAllRecyclableItem();
+
+            return View(recyclableItems);
+        }
+
+   
+        public ActionResult Add()
+        {
+            var recyclableTypes = recyclableTypeRepository.GetRecyclableTypes()
+            .Select(t => new SelectListItem
+            {
+                Value = t.Rate.ToString(),  
+                Text = t.Type  
+            });
+
+            var viewModel = new RecyclableItemViewModel
+            {
+                RecyclableTypes = recyclableTypes
+            };
+
+            return View(viewModel);
+
+ 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(RecyclableItemViewModel viewModel)
+        {
+            string type = viewModel.SelectedRecyclableTypeText;
+            if (ModelState.IsValid)
+            {
+ 
+                var recyclableType = recyclableTypeRepository.GetRecyclableTypeByType(type);
+
+                var typeId = recyclableType.Id;
+
+                if (recyclableType.MaxKg >= viewModel.RecyclableItem.Weight && 
+                    recyclableType.MinKg <= viewModel.RecyclableItem.Weight )
+                {
+                    var recyclableItem = new RecyclableItem
+                    {
+                        TypeId = typeId,
+                        Weight = viewModel.RecyclableItem.Weight,
+                        ComputedRate = viewModel.RecyclableItem.ComputedRate,
+                        ItemDescription = viewModel.RecyclableItem.ItemDescription
+                    };
+
+                    recyclableItemRepository.AddRecyclableItem(recyclableItem);
+
+                    return RedirectToAction("List");
+                }
+                
+            }
+
+            viewModel.RecyclableTypes = recyclableTypeRepository.GetRecyclableTypes()
+           .Select(t => new SelectListItem { Value = t.Rate.ToString(), Text = t.Type });
+
+            return View(viewModel);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+
+            RecyclableItem recyclableItem = recyclableItemRepository.GetRecyclableItem(id);
+
+            IEnumerable<RecyclableType> recyclableTypes = recyclableTypeRepository.GetRecyclableTypes();
+
+            RecyclableItemViewModel viewModel = new RecyclableItemViewModel
+            {
+                
+                SelectedRecyclableTypeId = id,
+                RecyclableItem = recyclableItem,
+                RecyclableTypes = recyclableTypes.Select(t => new SelectListItem
+                {
+                    Value = t.Rate.ToString(), 
+                    Text = t.Type  
+                })
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(RecyclableItemViewModel viewModel)
+        {
+          
+            string type = viewModel.SelectedRecyclableTypeText;
+
+            if (ModelState.IsValid)
+            {
+
+                var recyclableType = recyclableTypeRepository.GetRecyclableTypeByType(type);
+
+                var typeId = recyclableType.Id;
+
+                if (recyclableType.MaxKg >= viewModel.RecyclableItem.Weight &&
+                    recyclableType.MinKg <= viewModel.RecyclableItem.Weight)
+                {
+
+                    var recyclableItem = new RecyclableItem
+                    {
+                        Id = viewModel.SelectedRecyclableTypeId,
+                        TypeId = typeId,
+                        Weight = viewModel.RecyclableItem.Weight,
+                        ComputedRate = viewModel.RecyclableItem.ComputedRate,
+                        ItemDescription = viewModel.RecyclableItem.ItemDescription
+                    };
+
+
+                    recyclableItemRepository.EditRecyclableItem(recyclableItem);
+
+                    return RedirectToAction("List");
+                }
+                
+            }
+
+            viewModel.RecyclableTypes = recyclableTypeRepository.GetRecyclableTypes()
+          .Select(t => new SelectListItem { Value = t.Rate.ToString(), Text = t.Type });
+
+            return View(viewModel);
+
+            
+
+     
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+             
+            RecyclableItem recyclableItem = recyclableItemRepository.GetRecyclableItem(id);
+
+            if (recyclableItem == null)
+            {
+                 
+                return NotFound();
+            }
+
+            return View(recyclableItem);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmation(int id)
+        {
+
+            recyclableItemRepository.DeleteRecyclableItem(id);
+
+            return RedirectToAction("List");
+
+        }
+
+      
+    }
+}
---
+++ Controllers/RecyclableTypeController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Recyclable.Interface;
+using Recyclable.Models;
+using Recyclable.Repository;
+
+namespace Recyclable.Controllers
+{
+    public class RecyclableTypeController : Controller
+    {
+        public IRecyclableTypeRepository recyclableTypeRepository;
+        public RecyclableTypeController(IRecyclableTypeRepository _recyclableTypeRepository)
+        {
+            recyclableTypeRepository = _recyclableTypeRepository;
+        }
+
+ 
+        public IActionResult List()
+        {
+            IEnumerable<RecyclableType> recyclableTypes = recyclableTypeRepository.GetRecyclableTypes();
+
+            return View(recyclableTypes);
+        }
+
+ 
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+ 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(RecyclableType recyclableType)
+        {
+            if (recyclableType.MaxKg >= recyclableType.MinKg &&
+                recyclableType.MaxKg > 0 && recyclableType.MinKg > 0 &&
+                recyclableType.Rate > 0)
+            { 
+                recyclableTypeRepository.AddRecyclableType(recyclableType);
+
+                return RedirectToAction("List");
+            }
+
+            
+            return View(recyclableType);
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+
+            var recyclableType = recyclableTypeRepository.GetRecyclableType(id);
+
+
+            return View(recyclableType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(RecyclableType recyclableType)
+        {
+             
+            if (recyclableType.MaxKg >= recyclableType.MinKg &&
+                recyclableType.MaxKg > 0 && recyclableType.MinKg > 0 &&
+                recyclableType.Rate > 0)
+            {
+                recyclableTypeRepository.UpdateRecycleType(recyclableType);
+
+                return RedirectToAction("List");
+            }
+
+            return View(recyclableType);
+        }
+ 
+        public IActionResult ItemList(int id)
+        {
+            // Get recyclable items based on the type id
+            var recyclableItems = recyclableTypeRepository.GetRecyclableItems(id);
+
+             
+
+            return View(recyclableItems);
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+      
+            RecyclableType recyclableType= recyclableTypeRepository.GetRecyclableType(id);
+ 
+            if (recyclableType == null)
+            {
+                 
+                return NotFound();
+            }
+
+            return View(recyclableType);
+        }
+
+ 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmation(int id)
+        {
+
+            recyclableTypeRepository.DeleteRecycleType(id);
+
+
+            return RedirectToAction("List");
+
+        }
+    }
+}
